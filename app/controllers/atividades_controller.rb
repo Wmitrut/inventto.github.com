73eabf22281ccc0d534252248114187ea08af7aa
Edson <edsontofolo@gmail.com>
2013-01-17 14:20:17 -0200

    Criado o filtro por cliente, e arrumado para funcionar corretamente

--- app/controllers/atividades_controller.rb
+++ app/controllers/atividades_controller.rb
@@ -2,10 +2,10 @@ class AtividadesController < ApplicationController
   def ver
     if(params[:projeto].blank? and params[:cliente].blank?)
       @atividades = Atividade.all
-    else if (not params[:projeto].blank?)
+    elsif (not params[:projeto].blank?)
       @atividades = Atividade.find_all_by_projeto_id params[:projeto].to_i
-    else if not params[:cliente].blank?
-      @atividades = Atividade.find_all_by_projeto_id params[:projeto].to_i
+    elsif (not params[:cliente].blank?)
+      @atividades = Atividade.joins("JOIN projetos ON projetos.id=projeto_id JOIN clientes ON clientes.id=projetos.cliente_id").where(:"projetos.cliente_id" => params[:cliente].to_i)
     end
   end
   def lancar
