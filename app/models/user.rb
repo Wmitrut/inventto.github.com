73f2ba4662b70ae60fe3f5ac99f1a752d10b79c1
Lucas Cardoso de Araujo <lucas_garsa@hotmail.com>
2013-01-24 17:15:40 -0200

    Foi feito o login com interface na página principal

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :nome,:uid,:provider
+
   def self.from_omniauth(auth)
     find_by_provider_and_uid(auth["provider"], auth["uid"]) || create_with_omniauth(auth)
   end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-#OmniAuth.config.full_host = "http://4mcg.localtunnel.com"
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,"906135252788.apps.googleusercontent.com", "OEUPs5pxwzqz0O7V6XQbdM9Q"
+  provider :google_oauth2,"179111133108.apps.googleusercontent.com", "UwN-t_af2mX2mgKR3GrepCJg"
 end
