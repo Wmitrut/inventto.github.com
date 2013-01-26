8c30e92fc879136652c8c5f8b78807bb0bfdfbc1
Edson <edson@edson-pc.(none)>
2013-01-26 00:13:59 -0200

    Corrigidos erros que estavam ocorrendo ao criar uma nova base e suas tabelas

--- db/migrate/2013012237800_add_descricao_to_projeto.rb
+++
@@ -1,9 +0,0 @@
-class AddDescricaoToProjeto < ActiveRecord::Migration
-  def self.up
-    add_column :projetos, :descricao, :text
-  end
-
-  def self.down
-    remove_column :projetos, :descricao
-  end
-end
