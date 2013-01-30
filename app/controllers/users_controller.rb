c759ae47cf06f682d32a7658d99cc4e2aa627308
Lucas Cardoso de Araujo <lucas_garsa@hotmail.com>
2013-01-30 14:42:10 -0200

    Criado o campo developer na tabela de usuários. #18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   active_scaffold :user do |conf|
-    conf.columns.exclude :uid, :provider, :image
+    conf.columns.exclude :uid, :provider
     conf.list.columns.exclude :uid, :provider
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def image_column(model, column)
+    "<img src='#{model.image}' height='48'>".html_safe
+  end
+  def image_form_column(model, column)
+    "<img src='#{model.image}' height='48'>".html_safe
+  end
 end
