268e3827b2386e75385785db5d0c04ab0663511e
Edson Tofolo <edsontofolo@gmail.com>
2013-02-01 14:34:05 -0200

    export GOOGLE_KEY and GOOGLE_SECRET to connect to ominiauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '1041592713252.apps.googleusercontent.com','SN7Fkz_qrxvVV7JB7GK6pc-S'#Lucao's credentials -> "179111133108.apps.googleusercontent.com", "UwN-t_af2mX2mgKR3GrepCJg"
+  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
 end
