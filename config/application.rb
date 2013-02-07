e2d413cc02b837544367750a4e950b40bf782013
Jônatas Davi Paganini <jonatasdp@gmail.com>
2013-02-07 16:48:44 +0000

    assets in production fixing directory

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module InventtoSite
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
-    config.assets.precompile += ["default.js"]
+    config.assets.precompile += ["default.js", "application.css"]
     # Enable the asset pipeline
     config.assets.enabled = true
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,13 +12,14 @@ InventtoSite::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = false
+  config.assets.manifest = Rails.root.join("public/assets")
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
