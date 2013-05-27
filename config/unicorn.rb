595d4bc04a0c377330d1d86e7fe6c875acb6a7b7
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2013-05-27 11:33:55 -0300

    merged

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 worker_processes 1
 
-listen 3333 
+listen 3333
 
 preload_app true
 
@@ -10,6 +10,3 @@ pid               '/var/www/apps/inventto/shared/pids/unicorn.pid'
 stderr_path       '/var/www/apps/inventto/shared/log/unicorn.error.log'
 stdout_path       '/var/www/apps/inventto/shared/log/unicorn.out.log'
 working_directory '/var/www/apps/inventto/current'
-after_fork do |server, worker| 
-	ActiveRecord::Base.establish_connection 
-end
